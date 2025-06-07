8982f5d63a2c8882149f823f012664a4352bee06
Sergei Luckyday <sergeiluckyday@gmail.com>
2025-06-07 15:08:00 +0600

    3.1.1

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -3,9 +3,9 @@ package web.dao;
 import web.model.User;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
